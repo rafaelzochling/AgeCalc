8bc5d9cb6daf48044a5e1aef2cdb2a9c87ea6123
Rafael Zochling <rafaelzochling@gmail.com>
2018-06-23 01:56:56 -0300

    Removing unnecessary spaces.

--- calcScript.js
+++ calcScript.js
@@ -1,15 +1,13 @@
-
 function calcAge() {
   var date = document.getElementById('date').value;
   var today = new Date();
   var bDay = new Date(date);
-  
   var age = today.getFullYear() - bDay.getFullYear();
   var monthTest = today.getMonth() - bDay.getMonth();
+
   if (monthTest < 0 || (monthTest == 0 && today.getDate() < bDay.getDate() )) {
         age--;
     }
-
 
   document.getElementById('results').innerHTML = age;
 }
